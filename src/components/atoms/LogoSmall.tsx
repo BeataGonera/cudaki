5ee8535debe89edda534bf3b9c66e2baa639520b
Beata Gonera <beataiwonagonera@gmail.com>
2024-07-11 15:29:15 +0200

    logo bigger

--- src/components/atoms/LogoSmall.tsx
+++ src/components/atoms/LogoSmall.tsx
@@ -3,7 +3,10 @@ import Image from "next/image";
 
 const LogoSmall = () => {
   return (
-    <Link href="/" className="relative w-[107px] h-[51px] z-20 block">
+    <Link
+      href="/"
+      className="relative w-[107px] h-[51px] lg:w-[160px] lg:h-[76px] z-20 block"
+    >
       <Image
         src="/logosmall.webp"
         alt="Logo fundacji Cudak"
