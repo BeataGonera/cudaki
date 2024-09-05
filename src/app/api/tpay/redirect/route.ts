0713b0f450c366f4125f0f73fcc375d6227da1d0
Beata Gonera <beataiwonagonera@gmail.com>
2024-09-05 10:28:19 +0200

    update

--- src/app/api/tpay/redirect/route.ts
+++ src/app/api/tpay/redirect/route.ts
@@ -6,10 +6,10 @@ export async function POST(req: NextRequest) {
 
   // Replace these with your Tpay credentials
   const merchantId = `${process.env.TPAY_MERCHANT_ID!}`;
-  const secret = `${process.env.TPAY_SECRET_KEY!}`;
+  const secret = process.env.TPAY_SECRET_KEY!;
   const crc = ""; // Optional, you can leave it empty or use it for tracking purposes
   //   const resultUrl = "http://localhost:3000/api/tpay/verify"; // Your endpoint for Tpay notifications
-  const returnUrl = "https://cudaki.org/dziekujemy"; // Where the user will be redirected after payment
+  const returnUrl = "https://cudak.org/dziekujemy"; // Where the user will be redirected after payment
 
   // Create the MD5 hash signature required by Tpay
   const md5sum = crypto.createHash("md5");
